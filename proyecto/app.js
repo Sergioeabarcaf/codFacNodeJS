d66d927e66a092189e7631cb182ec92243dd5f56
sergioeabarcaf <sergioaf1991@gmail.com>
2017-06-19 19:03:47 -0400

    master UPDATE app.js: Se agrega el uso del bodyParser para poder leer los parametros.

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,8 +1,10 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 app.use("/estatico",express.static('public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","jade");
 
 app.get("/",function(req,res){
