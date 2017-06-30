dd59e7572abad240285a49698b70949f2a19f3d5
sergioeabarcaf <sergioaf1991@gmail.com>
2017-06-30 18:59:23 -0400

    dev UPDATE proyecto/app.js: Se agrega linea para que muestre la confirmacion del password por consola

--- proyecto/app.js
+++ proyecto/app.js
@@ -21,7 +21,7 @@ app.post("/users",function(req,res){
 											password: req.body.password,
 											password_confirmation: req.body.password_confirmation
 											});
-											
+	console.log(user.password_confirmation);										
 	user.save(function(){
 		res.send("Guardamos tus datos")
 	});
