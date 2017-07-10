9cace1f65f054f1ce93be9a9006091c97c675f49
sergioeabarcaf <sergioaf1991@gmail.com>
2017-07-10 15:20:55 -0400

    dev UPDATE models/user.js: Se agregan las validaciones al Schema de tipo string, number y enum

--- proyecto/models/user.js
+++ proyecto/models/user.js
@@ -4,14 +4,17 @@ var Schema = mongoose.Schema;
 mongoose.Promise = require('bluebird');
 mongoose.connect("mongodb://127.0.0.1/fotos");
 
+var tipo_sexo = ["M", "F"];
+
 
 var user_schema = new Schema({
   name: String,
-  username: String,
-  password: String,
-  age: Number,
-  email: String,
+  username: {type: String, require: true, maxlength: [50,"El tamaño maximo es de 50 caracteres"], minlength: [8,"el tamaño minimo es de 8 caracteres"]},
+  password: {type: String, minlength: [8, "el tamaño minimo es de 8 caracteres"]},
+  age: {type: Number, min: [1,"la edad minima es de 1 año"], max: [100,"la edad maxima es de 100 años"]},
+  email: {type: String, require: "El correo es obligatorio"},
   date_of_birth: Date
+  sexo: {type: String, enum: {values: tipo_sexo, message: "Solo se aceptan valores de M y F"}}
 });
 
 user_schema.virtual("password_confirmation").get(function(){
