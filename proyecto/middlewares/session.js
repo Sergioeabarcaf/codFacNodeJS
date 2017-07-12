00713bbc557820464b3a7d16ba913f541afeb2f5
sergioeabarcaf <sergioaf1991@gmail.com>
2017-07-12 13:00:45 -0400

    dev UPDATE middlewares/session.js: aun no se repara el error que bota el sistema con contraseñas erroneas

--- proyecto/middlewares/session.js
+++ proyecto/middlewares/session.js
@@ -2,13 +2,13 @@ var User = require("../models/user").User;
 
 module.exports = function(req,res,next){
   if(!req.session.user_id){
-    res.redirect("/login")
+    res.redirect("/login");
   }
   else {
     User.findById(req.session.user_id,function(err,user){
       if(err){
-        res.redirect("/login");
         console.log(err);
+        res.redirect("/login");
       }
       else {
         res.locals = {user : user}
