426e581afa6f435c32b53d97df2770becb8814e4
sergioeabarcaf <sergioaf1991@gmail.com>
2017-07-12 10:09:53 -0400

    dev UPDATE app.js: Se agrega a la session el id del registro en mongodb

--- proyecto/app.js
+++ proyecto/app.js
@@ -8,9 +8,14 @@ var app = express();
 app.use("/estatico",express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	secret: "5454sddsfdfsfdg54dfsfs"
+}));
+
 app.set("view engine","jade");
 
 app.get("/",function(req,res){
+	console.log(req.session.user_id);
 	res.render("index");
 });
 
@@ -43,9 +48,9 @@ app.post("/users",function(req,res){
 });
 
 app.post("/sessions",function(req,res){
-	User.find({email: req.body.email,password: req.body.password},function(err,docs){
-		console.log(docs);
-		res.send("Hola mundo");
+	User.findOne({email: req.body.email,password: req.body.password},function(err,user){
+		req.session.user_id = user._id;
+		res.send("hola mundo");
 	});
 });
 app.listen(8080);
