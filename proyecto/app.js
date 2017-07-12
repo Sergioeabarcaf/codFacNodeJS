1cd1666e9271f3741666773fbabf93e4c14a7dbb
sergioeabarcaf <sergioaf1991@gmail.com>
2017-07-12 10:28:04 -0400

    dev CREATE router: Se crean la rutas modulares y se implementan en app.js

--- proyecto/app.js
+++ proyecto/app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
 var session = require("express-session");
+var router_app = require("./routes_app");
 
 var app = express();
 
